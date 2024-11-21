9ec9a846f00e73fa3c52f86b00c7fe6d90828e44
rafiardinata <rafiardinata@gmail.com>
2024-11-22 00:22:41 +0700

    tambahan_fitur_link

--- src/components/Gambar.tsx
+++ src/components/Gambar.tsx
@@ -3,11 +3,11 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/t
 const Gambar = () => {
 
     const data = [
-        { src: "/icon/laravel.png", name: "Laravel" },
-        { src: "/icon/logoreact.png", name: "React" },
-        { src: "/icon/tailwind.png", name: "Tailwind" },
-        { src: "/icon/Photoshop.png", name: "Photoshop" },
-        { src: "/icon/Figma.png", name: "Figma" },
+        { src: "/icon/laravel.png",     name: "Laravel",    link: "https://laravel.com/" },
+        { src: "/icon/logoreact.png",   name: "React",      link: "https://react.dev/" },
+        { src: "/icon/tailwind.png",    name: "Tailwind",   link: "https://tailwindcss.com/" },
+        { src: "/icon/Photoshop.png",   name: "Photoshop",  link: "https://www.adobe.com/id_id/products/photoshop.html" },
+        { src: "/icon/Figma.png",       name: "Figma",      link: "https://www.figma.com/" },
     ]
 
     return (
@@ -15,13 +15,15 @@ const Gambar = () => {
             {data.map((value, i) => (
                 <Tooltip key={i} delayDuration={200}>
                     <TooltipTrigger asChild>
-                        <img
-                            src={value.src}
-                            alt={value.name}
-                            width={48}
-                            height={48}
-                            className='transform hover:-translate-y-1 transition opacity-50 hover:opacity-100 duration-400 cursor-pointer'
-                        />
+                        <a href={value.link} target="_blank">
+                            <img
+                                src={value.src}
+                                alt={value.name}
+                                width={48}
+                                height={48}
+                                className='transform hover:-translate-y-1 transition opacity-50 hover:opacity-100 duration-400 cursor-pointer'
+                            />
+                        </a>
                     </TooltipTrigger>
                     <TooltipContent>
                         <p>{value.name}</p>
