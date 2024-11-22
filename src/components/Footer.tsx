b336e46dd6e583b9379b5d6cdf540a4e97f6f0f6
rafiardinata <rafiardinata@gmail.com>
2024-11-22 17:35:21 +0700

    update-responsive-2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,24 +3,26 @@ import { Dot, Dribbble, Github, Instagram, Linkedin } from 'lucide-react'
 const Footer = () => {
     return (
         <footer className="absolute lg:w-full bottom-0 left-0 right-0">
-            <div className="bg-[#323232] py-5 px-16 flex items-center justify-between">
-                <div className="flex items-center text-white gap-2">
+            <div className="bg-[#323232] py-5 sm:px-16 px-1 flex items-center justify-between">
+                <div className="flex items-center text-white">
                     <Dot />
-                    <span className="font-700 font-medium">Follow Me</span>
-                    <a href="https://dribbble.com/rafiardinata" className='opacity-[50%] hover:opacity-100' target='_blank'>
-                        <Dribbble />
-                    </a>
-                    <a href="https://www.instagram.com/rafiardinata/" className='opacity-[50%] hover:opacity-100' target='_blank'>
-                        <Instagram />
-                    </a>
-                    <a href="https://github.com/rafiardinata" className='opacity-[50%] hover:opacity-100' target='_blank'>
-                        <Github />
-                    </a>
-                    <a href="https://www.linkedin.com/in/rafiardinata/" className='opacity-[50%] hover:opacity-100' target='_blank'>
-                        <Linkedin />
-                    </a>
+                    <span className="font-700 font-medium sm:text-[16px] text-[10px] pr-2">Follow Me</span>
+                    <div className='flex gap-2'>
+                        <a href="https://dribbble.com/rafiardinata" className='opacity-[50%] hover:opacity-100' target='_blank'>
+                            <Dribbble className='w-5 h-fit' />
+                        </a>
+                        <a href="https://www.instagram.com/rafiardinata/" className='opacity-[50%] hover:opacity-100' target='_blank'>
+                            <Instagram className='w-5 h-fit' />
+                        </a>
+                        <a href="https://github.com/rafiardinata" className='opacity-[50%] hover:opacity-100' target='_blank'>
+                            <Github className='w-5 h-fit' />
+                        </a>
+                        <a href="https://www.linkedin.com/in/rafiardinata/" className='opacity-[50%] hover:opacity-100' target='_blank'>
+                            <Linkedin className='w-5 h-fit' />
+                        </a>
+                    </div>
                 </div>
-                <div className="text-white font-700 font-semibold">
+                <div className="text-white font-700 font-semibold sm:text-[16px] text-[9px]">
                     &copy; Rafi Ardinata Riskiansyah
                 </div>
             </div>
