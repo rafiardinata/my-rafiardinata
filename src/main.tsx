798ab5331b691fe8d10b888d0fb2b7a27632d43a
rafiardinata <rafiardinata@gmail.com>
2025-03-07 03:41:38 +0700

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Routes/Routes.tsx'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from '@vercel/analytics/react';
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} future={{ v7_startTransition: true }} />
+      <Analytics/>
     </QueryClientProvider>
   </StrictMode>,
 )
